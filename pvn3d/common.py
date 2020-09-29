b7c72fb6963bf6de0583a26945aa707b03713bf1
saadehmd <saadehmd@gmail.com>
2020-09-29 19:46:14 +0300

    Modifications to config file for openDR dataset

--- pvn3d/common.py
+++ pvn3d/common.py
@@ -34,14 +34,14 @@ class Config:
         ensure_fd(self.log_eval_dir)
 
         self.n_total_epoch = 25
-        self.mini_batch_size = 24
-        self.num_mini_batch_per_epoch = 4000
-        self.val_mini_batch_size = 24
-        self.val_num_mini_batch_per_epoch = 125
+        self.mini_batch_size = 20
+        self.num_mini_batch_per_epoch = 87 # 4000
+        self.val_mini_batch_size = 20
+        self.val_num_mini_batch_per_epoch = 30 # 125
         self.test_mini_batch_size = 1
 
         self.n_sample_points = 8192 + 4096
-        self.n_keypoints = 8
+        self.n_keypoints = 16
         self.n_min_points = 400
 
         self.noise_trans = 0.05 # range of the random noise of translation added to the training data
@@ -84,11 +84,55 @@ class Config:
                 self.exp_dir,
                 'datasets/ycb/test_val_data_pts{}.pkl'.format(self.n_sample_points),
             )
+
+        elif self.dataset_name == 'openDR':
+            
+            self.n_objects = 10 + 1
+            self.n_classes = 10 + 1
+            self.od_sym_cls_ids = [1,2,3,5,7]
+            self.openDR_cls_lst_p = os.path.abspath(
+                os.path.join(
+                    self.exp_dir, 'datasets/openDR/dataset_config/classes.txt'
+                )
+            )
+            self.openDR_root = os.path.abspath(
+                os.path.join(
+                    self.exp_dir, 'datasets/openDR/openDR_dataset'
+                )
+            )
+            self.openDR_kps_dir = os.path.abspath(
+                os.path.join(
+                    self.exp_dir, 'datasets/openDR/openDR_object_kps/'
+                )
+            )
+            openDR_r_lst_p = os.path.abspath(
+                os.path.join(
+                    self.exp_dir, 'datasets/openDR/dataset_config/radius.txt'
+                )
+            )
+            
+            '''
+            self.preprocessed_testset_pth = os.path.abspath(
+                os.path.join(
+                    self.exp_dir,
+                    'datasets/openDR/openDR_Dataset/preprocessed_valtestset.pkl'
+                )
+            )'''
+
+            self.use_preprocess = True
+            self.openDR_r_lst = list(np.loadtxt(openDR_r_lst_p))
+            self.openDR_cls_lst = self.read_lines(self.openDR_cls_lst_p)
+            self.openDR_sym_cls_ids = [1, 2, 3 ,5]
+            self.openDR_test_pkl_p = os.path.join(
+                self.exp_dir,
+                'datasets/openDR/test_val_data_pts{}.pkl'.format(self.n_sample_points),
+            )
+            
         else: # linemod
             self.n_objects = 1 + 1
             self.n_classes = 1 + 1
             self.lm_cls_lst = [
-                1, 2, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15
+                1, 2, 4, 5, 6, 8, 9, 10, 11, 12, 13, 14, 15, 16
             ]
             self.lm_sym_cls_ids = [10, 11]
             self.lm_obj_dict={
@@ -105,6 +149,7 @@ class Config:
                 'iron':13,
                 'lamp':14,
                 'phone':15,
+                'piston':16
             }
             self.lm_id2obj_dict = dict(
                 zip(self.lm_obj_dict.values(), self.lm_obj_dict.keys())
@@ -136,6 +181,10 @@ class Config:
             self.val_nid_ptn = "/data/6D_Pose_Data/datasets/LINEMOD/pose_nori_lists/{}_real_val.nori.list"
 
         self.intrinsic_matrix = {
+            'custom': np.array([[554.25469119, 0.,         320.5],
+                                [0.,        554.25469119,  240.5],
+                                [0.,        0.,         1.]]),
+
             'linemod': np.array([[572.4114, 0.,         325.2611],
                                 [0.,        573.57043,  242.04899],
                                 [0.,        0.,         1.]]),
@@ -147,7 +196,10 @@ class Config:
                                 [0.      , 0.        , 1.0]], np.float32),
             'ycb_K2': np.array([[1077.836, 0.        , 323.7872],
                                 [0.      , 1078.189  , 279.6921],
-                                [0.      , 0.        , 1.0]], np.float32)
+                                [0.      , 0.        , 1.0]], np.float32),
+            'openDR': np.array([[554.25469119, 0.,         320.5],
+                                [0.,        554.25469119,  240.5],
+                                [0.,        0.,         1.]])
         }
 
     def read_lines(self, p):
